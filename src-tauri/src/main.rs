1893de6323096b06d2b976e4c2c936523ee3222e
some1chan <31833556+some1chan@users.noreply.github.com>
2023-08-02 02:07:34 -0500

    style: format with prettier

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,13 +7,13 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use notify::RecursiveMode;
+use notify_debouncer_mini::{new_debouncer, DebounceEventResult};
 use std::ffi::CString;
 use std::path::Path;
 use std::{thread::sleep, time::Duration};
-use notify_debouncer_mini::{new_debouncer, DebounceEventResult};
 use tauri::Manager;
 use winapi::um::winuser::{FindWindowA, SetForegroundWindow};
-use notify::{RecursiveMode};
 
 #[tauri::command]
 fn focus_window(window_name: String) -> String {
@@ -72,22 +72,21 @@ fn get_offset_from_game_settings() -> Result<f64, String> {
         return Err(format!("Could not find settings file at {}", settings_path));
     }
 
-
     let file = match std::fs::read_to_string(settings_path) {
         Ok(file) => file,
-        Err(e) => return Err(format!("Failed to read settings file: {}", e))
+        Err(e) => return Err(format!("Failed to read settings file: {}", e)),
     };
 
     let json: serde_json::Value = match serde_json::from_str(&file) {
         Ok(json) => json,
-        Err(e) => return Err(format!("Failed to parse settings file: {}", e))
+        Err(e) => return Err(format!("Failed to parse settings file: {}", e)),
     };
 
-    let offset =    match json["rhythmTrackerPositionOffset"].as_f64() {
+    let offset = match json["rhythmTrackerPositionOffset"].as_f64() {
         Some(offset) => offset,
-        None => return Err(format!("Failed to get offset from settings file"))
+        None => return Err(format!("Failed to get offset from settings file")),
     };
-    
+
     Ok(offset as f64)
 }
 
@@ -101,51 +100,52 @@ fn main() {
             tauri::async_runtime::spawn(async move {
                 let mut interval = tokio::time::interval(Duration::from_millis(3000));
 
-                let settings_path =  get_path_to_game_settings();
+                let settings_path = get_path_to_game_settings();
 
                 // check if the file exists
                 let exists = std::path::Path::new(&settings_path).exists();
-                
+
                 if !exists {
-                    println!("Could not find settings file at {}, not running the watcher", settings_path);
+                    println!(
+                        "Could not find settings file at {}, not running the watcher",
+                        settings_path
+                    );
                     return;
                 }
 
                 // Select recommended watcher for debouncer.
                 // Using a callback here, could also be a channel.
-                let mut debouncer = new_debouncer(Duration::from_millis(10), None, move |res: DebounceEventResult| {
-                    match res {
+                let mut debouncer = new_debouncer(
+                    Duration::from_millis(10),
+                    None,
+                    move |res: DebounceEventResult| match res {
                         Ok(_events) => {
-                         
-                         let offset = get_offset_from_game_settings();
- 
-                         match offset {
-                             Ok(offset) => {
-                                 handle
-                                     .emit_all(
-                                         "config_changed",
-                                         Some(offset),
-                                     )
-                                     .expect("failed to emit");
-                             },
-                             Err(e) => {
-                                 handle
-                                     .emit_all(
-                                         "config_changed",
-                                         Some(e),
-                                     )
-                                     .expect("failed to emit");
-                             }
-                         }
-                        },
+                            let offset = get_offset_from_game_settings();
+
+                            match offset {
+                                Ok(offset) => {
+                                    handle
+                                        .emit_all("config_changed", Some(offset))
+                                        .expect("failed to emit");
+                                }
+                                Err(e) => {
+                                    handle
+                                        .emit_all("config_changed", Some(e))
+                                        .expect("failed to emit");
+                                }
+                            }
+                        }
                         Err(e) => println!("watch error: {:?}", e),
-                     }
-                }).unwrap();
+                    },
+                )
+                .unwrap();
 
                 // Add a path to be watched. All files and directories at that path and
                 // below will be monitored for changes.
-                debouncer.watcher().watch(Path::new(&settings_path), RecursiveMode::Recursive).unwrap();
-
+                debouncer
+                    .watcher()
+                    .watch(Path::new(&settings_path), RecursiveMode::Recursive)
+                    .unwrap();
 
                 // Do I need this?
                 loop {
@@ -155,7 +155,12 @@ fn main() {
 
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![greet, focus_window, press_key, get_offset_from_game_settings])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            focus_window,
+            press_key,
+            get_offset_from_game_settings
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
